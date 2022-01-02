591091485585cf8ea80b9ebf91ee9b99dedc0c58
FotisVach <fotisvach4@gmail.com>
2022-01-02 17:20:27 +0200

    Initial commit - Maven project with spring boot

---
+++ src/main/java/com/javaee/fotis/FotisApplication.java
@@ -0,0 +1,13 @@
+package com.javaee.fotis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaee/fotis/FotisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaee.fotis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FotisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
