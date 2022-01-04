52a325c6ae8a2413fee591d0ac73926f414b283e
FotisVach <fotisvach4@gmail.com>
2022-01-05 01:50:40 +0200

    Test changes for better code covorage

--- src/test/java/com/javaee/fotis/rest/TestBookEndpoint.java
+++ src/test/java/com/javaee/fotis/rest/TestBookEndpoint.java
@@ -1,6 +1,9 @@
 package com.javaee.fotis.rest;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -12,9 +15,13 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.javaee.fotis.dao.BookDao;
 import com.javaee.fotis.po.Book;
+import com.javaee.fotis.service.BookService;
 
 /**
  * Test class for {@link BookEndpoint}
@@ -63,6 +70,11 @@ class TestBookEndpoint {
 		assertEquals(100, sut.findAll().size());
 
 		numberOfBooks--;
+		// Test functionality when the book is not exists
+		ResponseEntity<String> response = restTemplate.exchange("http://localhost:" + port + "/api/books/1001", 
+				HttpMethod.DELETE, null, String.class);
+		assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+		assertTrue(response.getBody().contains("Book id not found - 1001"));
 	}
 
 	/**
@@ -83,6 +95,18 @@ class TestBookEndpoint {
 		assertEquals(numberOfBooks.longValue(), books.length);
 		assertEquals(1L, books[0].getId());
 		assertEquals(100L, books[99].getId());
+	}
+	
+	/**
+	 * Test Method for {@link BookEndpoint#getBooks()}
+	 */
+	@Test
+	void test_getBooks2() {
+		BookEndpoint ep = new BookEndpoint();
+		ep.bookService = mock(BookService.class);
+		when(ep.bookService.findAll()).thenReturn(new ArrayList<Book>());
+		
+		assertTrue(ep.getBooks().getStatusCode() == HttpStatus.NO_CONTENT);
 	}
 
 	/**
