c682fd826d7ed314640a46e6bc22840a80575eab
FotisVach <fotisvach4@gmail.com>
2022-07-26 00:44:55 +0300

    Change config method
    Use java config to config the datasource.
    Hide the username, password from codebase, add these as enviroment variable

---
+++ src/main/java/com/javaee/fotis/ConfigurationUAT.java
@@ -0,0 +1,39 @@
+package com.javaee.fotis;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * UAT Config
+ */
+@Profile("uat")
+@Configuration
+public class ConfigurationUAT {
+	
+	@Value("${USER}")
+	String username;
+	
+	@Value("${PASS}")
+	String password;
+	
+	/**
+	 * Config UAT DB.
+	 * 
+	 * @return UAT datasource
+	 */
+	@SuppressWarnings("nls")
+	@Bean
+	public DataSource dataSource() {
+		return DataSourceBuilder.create()
+				.url("jdbc:postgresql://localhost:5432/uat")
+				.username(username)
+				.password(password)
+				.build();
+	}
+	
+}
